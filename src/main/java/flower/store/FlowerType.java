f907cbb43829fe432ca512d1d4bc20b8d140a997
Blueberrylipstick <juliasavchyn1@gmail.com>
2023-10-12 20:50:13 +0300

    test added and fixed

--- src/main/java/flower/store/FlowerType.java
+++ src/main/java/flower/store/FlowerType.java
@@ -1,16 +1,5 @@
 package flower.store;
 
 public enum FlowerType {
-    CHAMOMILE("chamomile"), ROSE("rose"), TULIP("tulip");
-
-    private final String stringRep;
-
-    FlowerType(String stringRep) {
-        this.stringRep = stringRep;
-    }
-
-    @Override
-    public String toString() {
-        return stringRep;
-    }
+    CHAMOMILE, ROSE, TULIP;
 }
